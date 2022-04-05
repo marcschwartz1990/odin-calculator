6a5a2dffa2ee1a68ccd4a4d7e3e05c1a208ec888
Marc Schwartz <marcschwartz1990@gmail.com>
2022-04-05 11:58:27 -0400

    Initial Commit: Calculator + Functions with no Functionality yet.

---
+++ script.js
@@ -0,0 +1,21 @@
+//BASIC MATH OPERATORS
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+function operate(operator, num1, num2) {
+    return operator(num1, num2);
+};
