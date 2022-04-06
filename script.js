f68e57430c91acbd1b2ef4c437ce286d961cb114
Marc Schwartz <marcschwartz1990@gmail.com>
2022-04-06 13:46:04 -0400

    All buttons register as clicked and log to console. Addition works, finish other operators.

--- script.js
+++ script.js
@@ -19,3 +19,56 @@ function divide(a, b) {
 function operate(operator, num1, num2) {
     return operator(num1, num2);
 };
+
+function updateDisplay() {
+    let display = document.querySelector('.display');
+    display.textContent = currentDisplay;
+};
+
+function clearDisplay() {
+    currentDisplay = '';
+};
+
+const numbers = document.querySelectorAll('[data-number]');
+const operators = document.querySelectorAll('[data-operator]');
+const clear = document.querySelector('[data-clear]');
+const equals = document.querySelector('[data-equals]')
+
+let currentDisplay = '';
+let previousNumberInput = '';
+let currentNumberInput = '';
+let calcOperation = null
+
+numbers.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button);
+        currentDisplay += button.textContent;
+        currentNumberInput = currentDisplay;
+        updateDisplay();
+    });
+});
+
+clear.addEventListener('click', () => {
+    console.log(clear);
+    clearDisplay()
+    updateDisplay()
+});
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        console.log(operator);
+        previousNumberInput = currentNumberInput;
+        if (operator.textContent === '+') {
+            calcOperation = add;
+        }
+        clearDisplay();
+    });
+});
+
+equals.addEventListener('click', () => {
+    console.log(equals);
+    let total = operate(calcOperation, Number(previousNumberInput), Number(currentNumberInput));
+    currentDisplay = total;
+    updateDisplay()
+    console.log(`total: ${total}`)
+});
